da78df14459c89883f7ae18c9dfbccaffb66a112
leiyoujia <47096111+leiyoujia@users.noreply.github.com>
2020-03-22 18:11:19 +0800

    更换组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
 <div>
-    <div>mini-account</div>
+    <!-- <div>mini-account</div> -->
 
-    <a-button @click='category'>查看数据</a-button>
+    <!-- <a-button @click='category'>查看数据</a-button> -->
 
-    <v-table
+    <!-- <v-table
         :columns = 'columns'
-        :table-data = 'billArr'
-        :show-vertical-border = 'false'>
-    </v-table>
+        :table-data = 'tableData'
+        :show-vertical-border = 'false'
+        :filter-method = 'filterMethod'
+        :cell-edit-done='cellEditDone'>
+    </v-table> -->
+    <!-- <vue-snotify></vue-snotify> -->
+    <a-table :columns="columns" :dataSource="billArr" class="table" bordered :pagination= 'false'>
+        <span slot="customTitle">Type</span>
+    </a-table>
 </div>
 </template>
 
@@ -23,10 +29,50 @@ export default {
         billCategory: [],
         tableData: [],
         columns: [
-            {field: 'type', title: 'type', width: 100},
-            {field: 'time', title: 'time', width: 100},
-            {field: 'category', title: 'category', width: 100},
-            {field: 'amount', title: 'amount', width: 100}
+            {
+                dataIndex: 'type',
+                key: 'type',
+                slots: { title: 'customTitle' },
+                width: 50
+                // scopedSlots: { customRender: 'type' },
+            },
+            {
+                dataIndex: 'time',
+                title: 'time',
+                width: 50,
+                // key: 'time',
+                // slots: { title: 'customTitle' },
+                // scopedSlots: { customRender: 'type' },
+                filters: [
+                    {
+                        text: '1月',
+                        value: '1',
+                    },
+                    {
+                        text: '2月',
+                        value: '2',
+                    },
+                    {
+                        text: '3月',
+                        value: '3',
+                    }],
+                onfilter:(value, record)=>{
+                    console.log('value',value)
+                    console.log('record',record)
+
+                    return record.time.split('/')[1].indexOf(value) === 0
+                }
+            },
+            {
+                dataIndex: 'category',
+                title: 'category',
+                width: 50
+            },
+            {
+                dataIndex: 'amount',
+                title: 'amount',
+                width: 50
+            },
         ],
     }),
     created() {
@@ -34,6 +80,8 @@ export default {
             .then(axios.spread((bills, categories) => {
                 this.show(bills,categories)
             }));
+        // this.Alert1()
+        //this.$snotify.success('Example body content');        
     },
     methods: {
         data: function(){
@@ -54,6 +102,7 @@ export default {
                     const localDate = new Date(parseInt(rowArray[1]));
                     rowArray[1] = localDate.toLocaleDateString();
                     rowArray[4] = localDate.getMonth() + 1;
+                    rowArray[5] = Math.abs(index)
                     // rowArray[1] = that.formatDate(rowArray[1]);
                     const matchedCategory = this.billCategory.map(item => {
                         return item.split(',');
@@ -64,9 +113,9 @@ export default {
                     rowArray[2] = matchedCategory[2];
                     // rowArray[2] = that.replaceCate(rowArray[2]);
                     if(rowArray[0] ==0){
-                        rowArray[0] = 1
+                        rowArray[0] = '支出'
                     }else{
-                        rowArray[0] = 0
+                        rowArray[0] = '收入'
                     }
                     // const str = rowArray.join();
                     // console.log('str',rowArray[0])
@@ -83,19 +132,47 @@ export default {
                     time: rowArray[1],
                     category: rowArray[2],
                     amount: rowArray[3],
-                    month: rowArray[4]
+                    month: rowArray[4],
+                    key: rowArray[5]
                 }
             });
             that.billArr.shift();
+            this.tableData = that.billArr            
             console.log('billArr: ',this.billArr)
-        }
-    },
-    formatDate(date){
-        const localDate = new Date(parseInt(date));
-        return localDate.toLocaleDateString();
-    },
-    replaceCate(id){
-        return this.billCategory.find(item=>item.id === id)
-    },
+        },
+        formatDate(date){
+            const localDate = new Date(parseInt(date));
+            return localDate.toLocaleDateString();
+        },
+        replaceCate(id){
+            return this.billCategory.find(item=>item.id === id)
+        },
+        // filterMethod(value){
+        //     console.log('bill',value)
+        //     let data = this.billArr;
+            
+        //     data = data.filter(item => {
+        //         // console.log(item);
+        //         // item.time.split('/')[1]
+        //         return item.time.split('/')[1] === value
+        //     })
+                
+
+            
+        //     console.log(data)
+        //     this.tableData = data
+        // },
+    }
+    
+    
 }
 </script>
+
+<style>
+    .table{
+        width: 400px;
+        
+    }
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'ant-design-vue';
+import { Button,Table } from 'ant-design-vue';
 import App from './App.vue'
 import { VTable, VPagination } from 'vue-easytable'
 import 'vue-easytable/libs/themes-base/index.css'
 
+
+
 Vue.config.productionTip = false
+Vue.use(Table)
+
 
 Vue.component(Button.name, Button)
 Vue.component(VTable.name, VTable)
